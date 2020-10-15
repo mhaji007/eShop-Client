4db719ba367c79017fbcf103fd0bee53073215d5
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-15 19:39:47 -0400

    Set up routes and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {Switch, Route} from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home';
 
-const App = () => (
-  <div>
-    <p>React App</p>
-  </div>
-);
+
+const App = () => {
+  return (
+    <Switch>
+      <Route path="/"  exact component ={Home}/>
+      <Route path="/login" exact component ={Login}/>
+      <Route path="/register" exact component ={Register}/>
+    </Switch>
+  )
+}
 
 export default App;
