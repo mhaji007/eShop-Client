e153ee0977b18f6242b17b0f033f8514156217fc
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-16 19:09:52 -0400

    Store Logged in user's email and token in Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Switch, Route} from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
@@ -8,7 +8,39 @@ import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import RegisterComplete from './pages/auth/RegisterComplete';
 
+// Access the currenlty logged in user
+import {auth} from './firebase';
+// Used for dispatching action and
+// payload to update redux store
+import {useDispatch} from 'react-redux';
+
+
+
+
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  // Check fire base auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async(user) => {
+      if(user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        console.log("user", user)
+
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token
+          }
+        })
+      }
+    });
+    return () => unsubscribe();
+  }, [])
+
   return (
     <>
     <Header/>
