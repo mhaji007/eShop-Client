eec6a4d26fe73bf2180e43bb706d0862193ec9e9
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-15 22:02:54 -0400

    Add react router link

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import { Menu } from 'antd';
 import { HomeOutlined, UserOutlined, UserAddOutlined, SettingOutlined } from '@ant-design/icons';
+import {Link} from 'react-router-dom';
+
 
 const { SubMenu, Item } = Menu; // Menu.SunMenu
 
@@ -14,19 +16,18 @@ const Header = () => {
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
       <Item key="home" icon={<HomeOutlined />}>
-        Home
+        <Link to="/">Home</Link>
       </Item>
       <Item key="register" icon={<UserAddOutlined/>} className="float-right">
-        Register
+      <Link to="/register">Register</Link>
       </Item>
       <Item key="login" icon={<UserOutlined/>} className="float-right">
-        Login
+      <Link to="/login">Login</Link>
       </Item>
       <SubMenu key="SubMenu" icon={<SettingOutlined/>} title="Username">
           <Item key="setting:1">Option 1</Item>
           <Item key="setting:2">Option 2</Item>
       </SubMenu>
-
     </Menu>
   );
 }
