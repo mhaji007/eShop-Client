c75c85f3cc0d7b92e94facda6d0ef4e5120d7ba2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-15 20:25:25 -0400

    Set up Ant Design navigation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import {Switch, Route} from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Home from './pages/Home';
+import Header from './components/nav/Header';
 
 
 const App = () => {
   return (
+    <>
+    <Header/>
     <Switch>
       <Route path="/"  exact component ={Home}/>
       <Route path="/login" exact component ={Login}/>
       <Route path="/register" exact component ={Register}/>
     </Switch>
+    </>
   )
 }
 
