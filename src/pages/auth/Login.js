cb3b23cba667a1947a925275c6dbe71d8750a385
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-19 21:28:32 -0400

    Set up login with Google

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,80 +1,193 @@
-import React, {useState, useEffect} from "react";
-import {auth} from '../../firebase';
-import {toast} from "react-toastify";
-import { MailOutlined} from '@ant-design/icons';
-import styles from "./Register.module.scss"
-import classnames from 'classnames';
-import {useDispatch} from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { auth, googleAuthProvider } from "../../firebase";
+import { toast } from "react-toastify";
+import { Button } from "antd";
+import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
+import styles from "./Login.module.scss";
+import classnames from "classnames";
+import { useDispatch } from "react-redux";
 
-const Login = ({history}) => {
-
+const Login = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const[loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
 
-
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
+      setLoading(true);
       // Log user in via Firebase
       const result = await auth.signInWithEmailAndPassword(email, password);
-      const {user} = result
-      const idTokenResult = await user.getIdTokenResult()
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
       dispatch({
-        type: 'LOGGED_IN_USER',
+        type: "LOGGED_IN_USER",
         payload: {
           email: user.email,
-          token: idTokenResult.token
-        }
+          token: idTokenResult.token,
+        },
       });
-      history.push('/')
-    } catch(error) {
+      history.push("/");
+    } catch (error) {
       console.log(error);
       toast.error(error.message);
       setLoading(false);
     }
+  };
 
-  }
+  const googleLogin = async () => {
+    auth
+      .signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        // toast.error(err.message);
+      });
+  };
 
   const loginForm = () => {
     return (
       <div>
-      <form autoComplete='off' className={styles.form} onSubmit={handleSubmit}>
-    <div className={styles.control}>
-      {/* <h1>
+        <form
+          autoComplete="off"
+          className={styles.form}
+          onSubmit={handleSubmit}
+        >
+          <div className={styles.control}>
+            {/* <h1>
         Join
       </h1> */}
-    </div>
-    <div className={classnames(styles.control, styles.blockCube, styles.blockInput)}>
-      <input name='email' placeholder='Email' type='email'  value={email} onChange={(e) => setEmail(e.target.value)} autoFocus/>
-      <div className={styles.bgTop}>
-        <div className={styles.bgInner}></div>
-      </div>
-      <div className={styles.bgRight}>
-        <div className={styles.bgInner}></div>
-      </div>
-      <div className={styles.bg}>
-        <div className={styles.bgInner}></div>
-      </div>
-    </div>
+          </div>
+          <div
+            className={classnames(
+              styles.control,
+              styles.blockCube,
+              styles.blockInput
+            )}
+          >
+            <input
+              name="email"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoFocus
+            />
+            <div className={styles.bgTop}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.bgRight}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.bg}>
+              <div className={styles.bgInner}></div>
+            </div>
+          </div>
 
-    <div className={classnames(styles.control, styles.blockCube, styles.blockInput)}>
-      <input name='password' placeholder= 'Password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-      <div className={styles.bgTop}>
-        <div className={styles.bgInner}></div>
-      </div>
-      <div className={styles.bgRight}>
-        <div className={styles.bgInner}></div>
-      </div>
-      <div className={styles.bg}>
-        <div className={styles.bgInner}></div>
-      </div>
-    </div>
+          <div
+            className={classnames(
+              styles.control,
+              styles.blockCube,
+              styles.blockInput
+            )}
+          >
+            <input
+              name="password"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className={styles.bgTop}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.bgRight}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.bg}>
+              <div className={styles.bgInner}></div>
+            </div>
+          </div>
 
-    <button className={classnames(styles.btn, styles.blockCube, styles.blockCubeHover)} type='submit' disabled = {!email||password.length<6}>
+          <button
+            className={classnames(
+              styles.btn,
+              styles.blockCube,
+              styles.blockCubeHover
+            )}
+            type="submit"
+            disabled={!email || password.length < 6}
+          >
+            <div className={styles.bgTop}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.bgRight}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.bg}>
+              <div className={styles.bgInner}></div>
+            </div>
+            <div className={styles.text}>
+              {<MailOutlined />} {""}Login
+            </div>
+          </button>
+        </form>
+      </div>
+    );
+  };
+
+  const handleGoogleSubmit = (e) =>{
+    e.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      {/* <div className="row"> */}
+      {/* <div className="col-md-6 offset-md-3"> */}
+
+      {loading ? <h4 className>Loading...</h4> : <h4></h4>}
+      {loginForm()}
+      <form autoComplete="off" className={styles.form} onSubmit={handleGoogleSubmit}>
+        <button
+          className={classnames(
+            styles.btn,
+            styles.blockCube,
+            styles.blockCubeHover,
+            styles.googleButton,
+            "mb-3"
+          )}
+          type="submit"
+          onClick={googleLogin}
+        >
+          <div className={styles.bgTop}>
+            <div className={styles.bgInner}></div>
+          </div>
+          <div className={styles.bgRight}>
+            <div className={styles.bgInner}></div>
+          </div>
+          <div className={styles.bg}>
+            <div className={styles.bgInner}></div>
+          </div>
+          <div className={styles.text}>
+            {<GoogleOutlined />} {""} Login with Google
+          </div>
+        </button>
+      </form>
+
+      {/* <button className={classnames(styles.btn, styles.blockCube, styles.blockCubeHover)} type='submit' disabled = {!email||password.length<6} onClick={googleLogin}>
       <div className={styles.bgTop}>
         <div className={styles.bgInner}></div>
       </div>
@@ -86,26 +199,13 @@ const Login = ({history}) => {
       </div>
       <div className={styles.text}>
     {<MailOutlined/> } {""}Login
-
       </div>
-    </button>
-  </form>
-  </div>
-    );
-  }
+    </button> */}
 
-  return (
-    <div className="container p-5">
-      {/* <div className="row"> */}
-        {/* <div className="col-md-6 offset-md-3"> */}
-
-          {loginForm()}
-        {/* </div> */}
+      {/* </div> */}
       {/* </div> */}
     </div>
-  )
-
-
+  );
 };
 
 export default Login;
