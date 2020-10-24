db78b4c760f54ee32db3e18df845519772de0b25
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-24 16:55:49 -0400

    Implement redirect countdown for protected routes

---
+++ src/components/routes/LoadingToRedirect.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const LoadingToRedirect = () => {
+  const [count, setCount] = useState(5);
+  // Since this component is not a
+  // route component (not wrapped in a switch component)
+  // history is not available on the props
+  let history = useHistory();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((currentCount) => --currentCount);
+    }, 1000);
+    // Redirect once count is equal to 0
+    count === 0 && history.push("/");
+    // Cleanup
+    return () => clearInterval(interval);
+  }, [count]);
+
+  return (
+    <div className="container p-5 text-center">
+      <p>Redirecting you in {count} seconds</p>
+    </div>
+  );
+};
+
+export default LoadingToRedirect;
