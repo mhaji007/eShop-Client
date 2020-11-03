3b91af307e1ebee3fe5283bd5721df629ba81c83
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-02 23:04:03 -0500

    Implement image resize functionality on client

--- src/components/forms/FileUpload.js
+++ src/components/forms/FileUpload.js
@@ -1,12 +1,36 @@
 import React from "react";
+import axios from "axios";
+import Resizer from "react-image-file-resizer";
+import { useSelector } from "react-redux";
 
 const FileUpload = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
   const fileUploadAndResize = (e) => {
     // console.log(e.target.files);
     // Resize image sent from server
-    // send the resized image back to server to upload to cloudinary
-    // store urls in images[] in the parent component state (ProductCreate)
+
+    let files = e.target.files; // 3
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          "base64"
+        );
+      }
+    }
   };
+
+  // send the resized image back to server to upload to cloudinary
+  // store urls in images[] in the parent component state (ProductCreate)
 
   return (
     <div className="row">
@@ -22,7 +46,6 @@ const FileUpload = () => {
           //accept all the files
           // that starts with images
           accept="images/*"
-
           onChange={fileUploadAndResize}
         />
       </label>
