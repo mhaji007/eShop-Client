a096d2148ec12f608089fc4fa41bdbe725a4684c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-05 13:02:49 -0500

    Implement product delete functionality

--- src/components/cards/AdminProductCard.js
+++ src/components/cards/AdminProductCard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./AdminProductCard.module.css";
 import noImage from "../../images/noImage.jpg";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { EditOutlined, DeleteOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 
-const AdminProductCard = ({ product }) => {
-  const { title, description, images } = product;
+const AdminProductCard = ({ product, handleRemove }) => {
+  // Destructure values to be displayed
+  // and slug (for deleting and editing)
+  const { title, description, images, slug } = product;
 
   return (
     <>
@@ -30,17 +32,22 @@ const AdminProductCard = ({ product }) => {
 
         <div className={styles.description}>
           <div className={styles.icon}>
-            <i className="fas fa-info-circle"></i>
+          <QuestionCircleOutlined  style={{fontSize:"15px"}} />
           </div>
           <div className={styles.contents}>
             {/* <h2 className={styles.detailHead}>VIP Tutorials</h2> */}
             <p className={styles.detailDescription}> {description.lenght>150?description.substring(0,150):description}</p>{" "}
             <div className={styles.actionContainer} style={{height:"30vh", display:"grid", justifyContent:"end", alignItems:"end", gridTemplateColumns:"1fr 1fr"}}>
+              {/* delete and edit handlers are not implemented here
+              because they need to update state. If there were implemented here
+              there would be no way to update the deleted or ediited product in
+              the state. So update and delete handlers are passed from the parent
+              component (AllProducts) as props */}
               <div className="mx-auto">
                 <EditOutlined style={{fontSize:"20px"}}  />
               </div>
               <div className="mx-auto">
-                <DeleteOutlined  style={{fontSize:"20px"}}/>
+                <DeleteOutlined  style={{fontSize:"20px"}} onClick={()=>handleRemove(slug)}/>
               </div>
             </div>
           </div>
--- src/pages/admin/product/AllProducts.js
+++ src/pages/admin/product/AllProducts.js
@@ -2,19 +2,29 @@ import React, { useEffect, useState } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { getProductsByCount } from "../../../functions/product";
 import AdminProductCard from "../../../components/cards/AdminProductCard";
+import { removeProduct } from "../../../functions/product";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  // Destructure user from redux state
+  // To be used for sending slug when updating an deleting
+  const { user } = useSelector((state) => ({ ...state }));
+
+  // Load all porducts upon component mounting
   useEffect(() => {
     loadAllProducts();
   }, []);
-
+  // Fetch all products
   const loadAllProducts = () => {
     setLoading(true);
+    // Fetch 100 products (this is done to prevent server overload)
     getProductsByCount(100)
       .then((res) => {
+        // Store products in local state
         setProducts(res.data);
         setLoading(false);
       })
@@ -24,11 +34,30 @@ const AllProducts = () => {
       });
   };
 
+  const handleRemove = (slug) => {
+    // Confirm deletion
+    // let answer = window.confirm("Delete?");
+    if (window.confirm("Delete?")) {
+      // console.log("send delete request", slug);
+      removeProduct(slug, user.token)
+        .then((res) => {
+          // Load all products again
+          // to update the view
+          loadAllProducts();
+          toast.error(` Product ${res.data.title} is deleted`);
+        })
+        .catch((err) => {
+          if (err.response.status === 400) toast.error(err.response.data);
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
-        {/* <div> */}
+          {/* <div> */}
           <AdminNav />
         </div>
         {loading ? (
@@ -36,16 +65,23 @@ const AllProducts = () => {
         ) : (
           <div className="col-md-10">
             <div className="row">
-          {products.map((product) => (
-            // <div key={product._id} className="col-md-4">
-            <div key={product._id}>
-              <AdminProductCard product={product} />
+              {products.map((product) => (
+                // <div key={product._id} className="col-md-4">
+                <div key={product._id}>
+                  <AdminProductCard
+                    product={product}
+                    // Send handleRemove to child component
+                    // Handle remove is implemented here
+                    // becauase if it were to be done
+                    // in the child component, the parent
+                    // would not be updated
+                    handleRemove={handleRemove}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
           </div>
-        </div>
         )}
-
       </div>
     </div>
   );
