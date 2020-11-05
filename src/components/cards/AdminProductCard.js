a0f6f98366975fd3f869a3f80a591bd185443594
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-05 00:36:12 -0500

    Add placeholder image for products with no image. Trim product description. Modify product card styling. Add edit and delete icons

--- src/components/cards/AdminProductCard.js
+++ src/components/cards/AdminProductCard.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 import styles from "./AdminProductCard.module.css";
+import noImage from "../../images/noImage.jpg";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
- const AdminProductCard = ({product}) => {
-
-  const {title, description, images} = product;
+const AdminProductCard = ({ product }) => {
+  const { title, description, images } = product;
 
   return (
-<>
-	<div className={styles.card}>
-		<div className={styles.imgBox}>
-			<img src={images && images.length ? images[0].url:""}/>
-		</div>
-		<div className={styles.details}>
-			<div className={styles.textContent}>
-				<h3 className={styles.name}>{title}</h3>
-				<div className={styles.price}>£250</div>
-			</div>
-			{/* <h4 className={styles.colors}>Colors</h4>
+    <>
+      <div className={styles.card}>
+        <div className={styles.imgBox}>
+          <img src={images && images.length ? images[0].url : noImage} />
+        </div>
+        <div className={styles.details}>
+          <div className={styles.textContent}>
+            <h3 className={styles.name}>{title}</h3>
+            <div className={styles.price}>£250</div>
+          </div>
+          {/* <h4 className={styles.colors}>Colors</h4>
 			<ul className={styles.cardUl}>
 				<li></li>
 				<li></li>
@@ -25,26 +26,28 @@ import styles from "./AdminProductCard.module.css";
 				<li></li>
 			</ul>
 			<button>Buy Now</button> */}
-		</div>
+        </div>
 
-		<div className={styles.description}>
-			<div className={styles.icon}><i className="fas fa-info-circle"></i></div>
-			<div className={styles.contents}>
-				{/* <h2 className={styles.detailHead}>VIP Tutorials</h2> */}
-    <p className={styles.detailDescription}> {description}</p> </div>
-		</div>
-
-
-
-
-
-
-
-	</div>
-</>
-  )
- }
-
-
+        <div className={styles.description}>
+          <div className={styles.icon}>
+            <i className="fas fa-info-circle"></i>
+          </div>
+          <div className={styles.contents}>
+            {/* <h2 className={styles.detailHead}>VIP Tutorials</h2> */}
+            <p className={styles.detailDescription}> {description.lenght>150?description.substring(0,150):description}</p>{" "}
+            <div className={styles.actionContainer} style={{height:"30vh", display:"grid", justifyContent:"end", alignItems:"end", gridTemplateColumns:"1fr 1fr"}}>
+              <div className="mx-auto">
+                <EditOutlined style={{fontSize:"20px"}}  />
+              </div>
+              <div className="mx-auto">
+                <DeleteOutlined  style={{fontSize:"20px"}}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default AdminProductCard;
