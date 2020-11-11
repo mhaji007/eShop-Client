2f25246ce3304101e49eaeed3d12e4c2b26824f9
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-11 14:19:30 -0500

    Set up initial components for displaying product detail

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { getProduct } from "../functions/product";
+import SingleProduct from "../components/cards/SingleProduct";
 
 const Product = ({ match: {params:{slug}} }) => {
   const [product, setProduct] = useState({});
+
 
   useEffect(() => {
     loadSingleProduct();
@@ -11,7 +13,17 @@ const Product = ({ match: {params:{slug}} }) => {
   const loadSingleProduct = () =>
     getProduct(slug).then((res) => setProduct(res.data));
 
-  return <>{JSON.stringify(product)}</>;
+  return (
+    <div className="container-fluid">
+      <div className="row pt-4">
+        <SingleProduct product={product} />
+      </div>
+
+      <div className="row">
+        <div>Related products</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
