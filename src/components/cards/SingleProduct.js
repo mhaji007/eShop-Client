c405e727c08f37bde3da236dcd12566bc8b91102
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-11 16:03:21 -0500

    Implement image carousel in product detail page

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -2,15 +2,20 @@ import React from "react";
 import { Card } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const { Meta } = Card;
 
 const SingleProduct = ({ product }) => {
   const { title, description, images, slug } = product;
-
   return (
     <>
-      <div className="col-md-7">image carousel</div>
+      <div className="col-md-7">
+        <Carousel showArrows={true} autoPlay infiniteLoop>
+          {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+        </Carousel>
+      </div>
 
       <div className="col-md-5">
         <Card
