fd3c44427077f69a13b6b58c8249c578aa274e05
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-13 13:04:19 -0500

    Implement conditional rendering of ratings modal based on user login status

--- src/components/modal/RatingModal.js
+++ src/components/modal/RatingModal.js
@@ -3,19 +3,39 @@ import { Modal, Button } from "antd";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { StarOutlined } from "@ant-design/icons";
+// Since this component is not a route component
+// we do not have access to history in the props
+// hence, the use of useHistory hook here
+import {useHistory} from 'react-router-dom';
 
 const RatingModal = ({ children }) => {
   const { user } = useSelector((state) => ({ ...state }));
   const [modalVisible, setModalVisible] = useState(false);
 
+  let history = useHistory();
+
+  // Function for conditonally rendering ratings modal
+  // based on user's login status
+  const handleModal = () => {
+    if (user && user.token) {
+      setModalVisible(true);
+    } else {
+      history.push("/login");
+    }
+  }
+
   return (
     <>
-      <div onClick={() => setModalVisible(true)}>
+      {/* <div onClick={() => setModalVisible(true)}> */}
+      {/* Instead of always displaying the model on click
+       regardless of log in statues, conditionally render
+       the modal  */}
+      <div onClick={handleModal}>
         <StarOutlined className="text-danger" /> <br />{" "}
-        {user ? "Leave rating" : "Login to leave rating"}
+        {user ? "Leave a rating" : "Login to leave a rating"}
       </div>
       <Modal
-        title="Leave rating"
+        title="Leave a rating"
         centered
         visible={modalVisible}
         onOk={() => {
