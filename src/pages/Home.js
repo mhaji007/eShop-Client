c74969a36b1e3c9eb252373dff3d782316044510
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-14 17:10:02 -0500

    Implement category list functionality in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 // import { getProductsByCount } from "../functions/product";
-import { getProducts, getProductsCount} from "../functions/product";
+import { getProducts, getProductsCount } from "../functions/product";
 import ProductCard from "../components/cards/ProductCard";
 import Loader from "../components/loader/Loader";
 import Jumbotron from "../components/cards/Jumbotron";
 import LoadingCard from "../components/cards/LoadingCard";
 import styles from "./Home.module.scss";
-import {Pagination} from "antd";
-
+import { Pagination } from "antd";
+import CategoryList from "../components/category/CategoryList";
 
 const Home = () => {
   const [arrivals, setArrivals] = useState([]);
@@ -33,84 +33,80 @@ const Home = () => {
     });
   };
 
-    useEffect(() => {
+  useEffect(() => {
     loadAllBestSellersProducts();
   }, [bestSellersPage]);
 
   const loadAllBestSellersProducts = () => {
     setLoading(true);
 
-      getProducts("sold", "desc", bestSellersPage).then((res) => {
+    getProducts("sold", "desc", bestSellersPage).then((res) => {
       setBestSellers(res.data);
       setLoading(false);
     });
   };
 
-
-  useEffect (() => {
-    getProductsCount().then ((res) => setArrivalsProductsCount(res.data));
-    getProductsCount().then ((res) => setBestSellersCount(res.data));
-  }, [] )
-
-
+  useEffect(() => {
+    getProductsCount().then((res) => setArrivalsProductsCount(res.data));
+    getProductsCount().then((res) => setBestSellersCount(res.data));
+  }, []);
 
   return (
     <>
-    {/* Jumbotron */}
+      {/* Jumbotron */}
+      <div>{loading ? <Loader /> : <Jumbotron />}</div>
+      {/* New Arrivals Section  */}
+      <h4 className="text-center p-3 mt-5 mb-5 display-3 ">New Arrivals</h4>
+
       <div>
-        {loading ? <Loader /> : <Jumbotron/>}
-      </div>
-    {/* New Arrivals Section  */}
-      <h4 className="text-center p-3 mt-5 mb-5 display-3 ">
-        New Arrivals
-      </h4>
-
-      <div >
         {loading ? (
           <LoadingCard count={3} />
         ) : (
-        <div className="row m-0 mb-3 justify-content-center ">
-          {arrivals.map((product) => (
-            <div key={product._id}>
-
-              <ProductCard product={product}/>
-            </div>
-          ))}
-        </div>
-         )}
+          <div className="row m-0 mb-3 justify-content-center ">
+            {arrivals.map((product) => (
+              <div key={product._id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
 
+      <Pagination
+        className="row m-0 mb-3 justify-content-center"
+        current={arrivalsPage}
+        total={(arrivalsProductsCount / 3) * 10}
+        onChange={(value) => setArrivalsPage(value)}
+      />
 
+      {/* Best sellers Section  */}
 
+      <h4 className="text-center p-3 mt-5 mb-5 display-3 ">Best Sellers</h4>
 
-    <Pagination className="row m-0 mb-3 justify-content-center" current = {arrivalsPage} total={(arrivalsProductsCount/3) * 10} onChange={(value) => setArrivalsPage(value)} />
-
-
-       {/* Best sellers Section  */}
-
-
-
-             <h4 className="text-center p-3 mt-5 mb-5 display-3 ">
-        Best Sellers
-      </h4>
-
-      <div >
+      <div>
         {loading ? (
           <LoadingCard count={3} />
         ) : (
-        <div className="row m-0 mb-3 justify-content-center ">
-          {bestSellers.map((product) => (
-            <div key={product._id}>
-
-              <ProductCard product={product}/>
-            </div>
-          ))}
-        </div>
-         )}
+          <div className="row m-0 mb-3 justify-content-center ">
+            {bestSellers.map((product) => (
+              <div key={product._id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
 
+      <Pagination
+        className="row m-0 mb-3 justify-content-center"
+        current={bestSellersPage}
+        total={(bestSellersCount / 3) * 10}
+        onChange={(value) => setBestSellersPage(value)}
+      />
 
-<Pagination className="row m-0 mb-3 justify-content-center" current = {bestSellersPage} total={(bestSellersCount/3) * 10} onChange={(value) => setBestSellersPage(value)} />
+      <h4 className="text-center p-3 mt-2 mb-5 display-3">Categories</h4>
+
+      <CategoryList />
 
     </>
   );
