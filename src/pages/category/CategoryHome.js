324987072ed23273721f730186729138caa5505a
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-15 12:42:39 -0500

    Set up CategoryHome page

--- src/pages/category/CategoryHome.js
+++ src/pages/category/CategoryHome.js
@@ -9,6 +9,7 @@ import { getCategory } from "../../functions/category";
 import { Link } from "react-router-dom";
 import ProductCard from "../../components/cards/ProductCard";
 import CategoryList from "../../components/category/CategoryList";
+import Loader from "../../components/loader/Loader";
 
 const CategoryHome = ({ match }) => {
   // State for storing the single category
@@ -23,13 +24,39 @@ const CategoryHome = ({ match }) => {
     setLoading(true);
     // Send slug to detemine which category
     // we want
-    getCategory(slug).then((c) => {
-      console.log(JSON.stringify(c.data, null, 4));
-      setCategory(c.data);
+    getCategory(slug).then((res) => {
+      console.log(JSON.stringify(res, null, 4));
+      setCategory(res.data.category);
+      setProducts(res.data.products);
+      setLoading(false);
     });
   }, []);
 
-  return <p>{slug}</p>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          {loading ? (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 ">
+              <Loader/>
+            </h4>
+          ) : (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 ">
+              {products.length} Products in "{category.name}" category
+            </h4>
+          )}
+        </div>
+      </div>
+
+      <div className="row m-0 mb-3 justify-content-center">
+        {products.map((p) => (
+          <div key={p._id}>
+            <ProductCard product={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryHome;
