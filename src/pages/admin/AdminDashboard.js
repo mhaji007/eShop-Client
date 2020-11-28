72ad86198341be0912bf62f25657005b42616295
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-11-28 12:34:59 -0500

    Load orders in admin dashboard

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,24 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../components/nav/AdminNav";
-
-
+import { getOrders, changeStatus } from "../../functions/admin";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
 const AdminDashboard = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
 
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+  const loadOrders = () =>
+    getOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then((res) => {
+      toast.success("Status updated");
+      loadOrders();
+    });
+  };
 
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
-        {/* <div> */}
           <AdminNav />
         </div>
+
         <div className="col">
           <h4>Admin Dashboard</h4>
+          {JSON.stringify(orders)}
         </div>
-
       </div>
     </div>
   );
